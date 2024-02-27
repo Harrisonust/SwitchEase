04bcf9ba5efcd56c26bd7eea9ba4ac309b8f2cd6
Harrisonust <henhen.luo@gmail.com>
2024-02-27 21:03:08 +0800

    rename and update doc

--- main/blink/blink.c
+++ main/blink/blink.c
@@ -5,15 +5,15 @@ static const char* TAG		   = "Blink task";
 
 void blink_init(void) {
 	ESP_LOGI(TAG, "Init LED!");
-	gpio_reset_pin(LED_PIN);
+	gpio_reset_pin(LED_GPIO);
 	/* Set the GPIO as a push/pull output */
-	gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+	gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
 }
 
 void blink_task(void* par) {
 	while(1) {
 		ESP_LOGI(TAG, "alive!");
-		// gpio_set_level(LED_PIN, s_led_state);
+		// gpio_set_level(LED_GPIO, s_led_state);
 		// s_led_state = !s_led_state;
 		vTaskDelay(1000 / portTICK_PERIOD_MS);
 	}
